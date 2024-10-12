67d0af8d051de05da5f9e696c933687624ba5f56
SIMBA <naaro2930@gmail.com>
2024-10-12 13:23:39 +0300

    fixed back

--- StudBack/database.py
+++ StudBack/database.py
@@ -7,16 +7,16 @@ from sqlalchemy.ext.declarative import declarative_base
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./MySiteDB.db"
 
-# SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@db:5432/admin"
+SQLALCHEMY_DATABASE_URL = "postgresql://admin:admin@db:5432/admin"
 
-# engine = create_engine(
-    # SQLALCHEMY_DATABASE_URL
-# )
-
-SQLALCHEMY_DATABASE_URL = "sqlite:///./MySiteDB.db"
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
+#
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./MySiteDB.db"
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+# )
 
 
 session = sessionmaker(bind=engine, expire_on_commit=False)
